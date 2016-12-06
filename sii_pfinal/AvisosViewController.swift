ba97301126abaaa6e758d126c3e987071cc104de
Majo Molina <mjjmm@live.com.mx>
2016-12-05 23:46:08 -0800

    vistas mal

---
+++ sii_pfinal/AvisosViewController.swift
@@ -0,0 +1,35 @@
+//
+//  AvisosViewController.swift
+//  sii_pfinal
+//
+//  Created by Majo Molina on 05/12/16.
+//  Copyright © 2016 Majo. All rights reserved.
+//
+
+import UIKit
+
+class AvisosViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destinationViewController.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
